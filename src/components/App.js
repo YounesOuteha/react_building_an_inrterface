56e14174d0ec0dc97bd5997be7caff2d93457e7a
YounesOuteha <younes.outeha@gmail.com>
2020-11-04 10:16:36 +0100

    Finalise the project

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import AddAppointment from './AddAppointments';
 import ListApointment from './ListApointment';
 import SearchApointment from './SearchAppointment';
 import '../css/App.css'
+import { findIndex, without } from 'lodash';
+import { each } from 'jquery';
 
 class App extends Component {
 
@@ -11,8 +13,66 @@ class App extends Component {
     this.state = {
       myName: 'Younes',
       myAppointments:[],
+      formDisplay: false,
+      orderBy:'petName',
+      orderDir:'asc',
+      queryText:'',
       lastIndex: 0 
-    }
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.AddAppointment = this.AddAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    })
+  }
+
+  searchApts(query){
+    this.setState({
+      queryText:query
+    })
+  }
+
+  changeOrder(order, dir){
+    this.setState({
+      orderBy:order,
+      orderDir: dir
+    })
+  }
+
+  updateInfo(name, value, id){
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments,{
+      aptId:id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments:tempApts
+    })
+  }
+
+  AddAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    apt.aptId= this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments:tempApts,
+      lastIndex: this.state.lastIndex + 1
+    })
+  }
+
+  deleteAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+    this.setState({
+      myAppointments: tempApts
+    })
   }
 
   componentDidMount() {
@@ -32,15 +92,55 @@ class App extends Component {
   
 render() {
   
+  let order;
+  let filteredApts = this.state.myAppointments;
+  if (this.state.orderDir=== 'asc'){
+    order = 1;
+  }else{
+    order = -1;
+  }
+
+  filteredApts = filteredApts.sort((a,b)=>{
+    if (a[this.state.orderBy].toLowerCase()<
+    b[this.state.orderBy].toLowerCase()
+    ){
+      return -1 * order;
+    }else{
+      return 1 * order;
+    }
+  }).filter(eachItem => {
+    return(
+      eachItem['petName']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase()) ||
+      eachItem['ownerName']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase()) ||
+      eachItem['aptNotes']
+      .toLowerCase()
+      .includes(this.state.queryText.toLowerCase())
+    )
+  })
+
   return (
     <main className="page bg-white" id="petratings">
     <div className="container">
       <div className="row">
         <div className="col-md-12 bg-white">
           <div className="container">
-            <AddAppointment />
-            <SearchApointment/>
-            <ListApointment appointments={this.state.myAppointments}/>
+            <AddAppointment 
+            formDisplay={this.state.formDisplay} 
+            toggleForm={this.toggleForm}
+            AddAppointment = {this.AddAppointment}/>
+            <SearchApointment 
+            orderBy={this.state.orderBy}
+            orderDir={this.state.orderDir}
+            changeOrder={this.changeOrder}
+            searchApts={this.searchApts}/>
+            <ListApointment 
+            appointments={filteredApts} 
+            deleteAppointment={this.deleteAppointment}
+            updateInfo={this.updateInfo}/>
           </div>
         </div>
       </div>
--- src/components/ListApointment.js
+++ src/components/ListApointment.js
@@ -9,23 +9,38 @@ export default class ListApointment extends Component {
                 {this.props.appointments.map(item =>(
                     <div className="pet-item col media py-3" key={item.aptId}>
                     <div className="mr-3">
-                        <button className="pet-delete btn btn-sm btn-danger">
+                        <button className="pet-delete btn btn-sm btn-danger"
+                        onClick={()=> this.props.deleteAppointment(item)}>
                             <FaTimes />
                         </button>
                     </div>
         
                     <div className="pet-info media-body">
                         <div className="pet-head d-flex">
-                        <span className="pet-name">{item.aptId}---{item.petName}</span>
+                        <span className="pet-name" 
+                        contentEditable 
+                        suppressContentEditableWarning
+                        onBlur={
+                            e => this.props.updateInfo('petName', e.target.innerText, item.aptId)
+                        }>{item.aptId}---{item.petName}</span>
                         <span className="apt-date ml-auto">
                             <Momemt date={item.aptDate} parse="YYYY-MM-dd hh:mm" format="MMM-D h:mma"/>
                         </span>
                         </div>
                         <div className="owner-name">
                         <span className="label-item">Owner: </span>
-                        <span>{item.ownerName}</span>
+                        <span contentEditable 
+                        suppressContentEditableWarning
+                        onBlur={
+                            e => this.props.updateInfo('ownerName', e.target.innerText, item.aptId)
+                        }>{item.ownerName}</span>
                         </div>
-                        <div className="apt-notes">{item.aptNotes}</div>
+                        <div className="apt-notes"
+                        contentEditable 
+                        suppressContentEditableWarning
+                        onBlur={
+                            e => this.props.updateInfo('aptNotes', e.target.innerText, item.aptId)
+                        }>{item.aptNotes}</div>
                     </div>
                     </div>
                 ))}
